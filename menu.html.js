44bde106211eeed64501acf6770bda042f3fa67c
unknown <uchitha.r@gmail.com>
2014-09-06 23:19:25 +0800

    Initial commit

---
+++ menu.html.js
@@ -0,0 +1,8 @@
+		<!-- Navigation Starts -->
+		<!-- Note down the syntax before editing. It is the default twitter bootstrap navigation -->
+		var element = "<nav class='collapse navbar-collapse bs-navbar-collapse' role='navigation'><ul class='nav navbar-nav'><li><a href='index.html'>Home</a></li><li class='dropdown'><a href='#' class='dropdown-toggle' data-toggle='dropdown'>Values<b class='caret'></b></a><ul class='dropdown-menu'><li><a href='support.html'>Vision</a></li><li><a href='vision.html'>Mission</a></li></ul></li><li><a href='service.html'>Services</a></li><li><a href='about.html'>About Us</a></li><li><a href='contact.html'>Contact</a></li></ul></nav>";
+
+		var node = document.getElementById('menu');
+		node.innerHTML = element;
+			
+		<!--/ Navigation Ends -->  
